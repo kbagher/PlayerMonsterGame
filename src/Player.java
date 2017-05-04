71633b792b88ab140a7b2ab6697d2514e24cdbd2
Khalid Dowaid <khalidowaid@gmail.com>
2017-05-04 18:36:29 +1000

    add increaseNougats add getCalories edit the player class (move method) count the first nougate

--- src/Player.java
+++ src/Player.java
@@ -3,110 +3,120 @@ import java.io.*;
 /*  This class encapsulates player position and direction
  */
 public class Player extends Moveable implements PlayerSkills, Serializable {
-    private boolean readyToStart = false;
-    private int steps;
-    private int calories;
-    private Trap trap;
+	private boolean readyToStart = false;
+	private int steps;
+	private int calories;
+	private Trap trap;
 
-    public Player(Grid g, Trap t, int row, int col, int caloriesValue) throws Exception {
-        super(g);
-        currentCell = grid.getCell(row, col);
-        currentDirection = ' ';
-        calories = caloriesValue;
-        trap = t;
-    }
+	public Player(Grid g, Trap t, int row, int col, int caloriesValue) throws Exception {
+		super(g);
+		currentCell = grid.getCell(row, col);
+		currentDirection = ' ';
+		calories = caloriesValue;
+		trap = t;
+	}
 
-    public Cell move() {
-        if (canPerformEnergyAction(calculateCalories(steps))) {
-            if (currentDirection != ' ') {
-                System.out.println(calories);
-                Cell tempcell = grid.getCell(currentCell, currentDirection, steps);
-                steps = grid.distance(currentCell, tempcell);
-                calories = reduceCalories(calculateCalories(steps));
-                //System.out.println(calories+"After "+ steps);
-                
-                    if (tempcell!= null)
-                    	currentCell = tempcell;
-                    if (currentCell.nougat.isConsumed()) {
-                        calories += currentCell.nougat.getValue();
-                        steps =1;
-                        return currentCell;
-                    }
-                    calories += currentCell.nougat.getValue();
-                    currentCell.nougat.setConsumed();
-                    steps =1;
-                    return currentCell;
-                
-            }
-            steps =1;
-            return currentCell;
-        }
-        steps=1;
-        return currentCell;
-    }
+	public Cell move() {
+		if (currentCell.nougat.isConsumed())
+			calories = increaseCalories(currentCell.nougat.getValue());
+		if (canPerformEnergyAction(calculateCalories(steps))) {
+			if (currentDirection != ' ') {
+				System.out.println(calories);
+				Cell tempcell = grid.getCell(currentCell, currentDirection, steps);
+				steps = grid.distance(currentCell, tempcell);
+				calories = reduceCalories(calculateCalories(steps));
+				// System.out.println(calories+"After "+ steps);
 
-    public int calculateCalories(int steps) {
-        int sum = 0;
-        for (int i = 1; i <= steps; i++) {
-            sum += Math.pow(Settings.STEP_CONSUMED_CALORIES, i);
+				if (tempcell != null)
+					currentCell = tempcell;
+				if (currentCell.nougat.isConsumed()) {
+					calories = increaseCalories(currentCell.nougat.getValue());
+					steps = 1;
+					return currentCell;
+				}
+				calories += currentCell.nougat.getValue();
+				currentCell.nougat.setConsumed();
+				steps = 1;
+				return currentCell;
 
-        }
-        return sum;
-    }
+			}
+			steps = 1;
+			return currentCell;
+		}
+		steps = 1;
+		return currentCell;
+	}
 
-    public boolean canPerformEnergyAction(int requiredCalories) {
-        if (requiredCalories <= calories)
-            return true;
-        return false;
-    }
+	public int calculateCalories(int steps) {
+		int sum = 0;
+		for (int i = 1; i <= steps; i++) {
+			sum += Math.pow(Settings.STEP_CONSUMED_CALORIES, i);
 
-    public int maxCellsPerMove() {
-        return 1;
-    }
+		}
+		return sum;
+	}
 
-    public int pointsRemaining() {
-        return -1;  // not implemented
-    }
+	public boolean canPerformEnergyAction(int requiredCalories) {
+		if (requiredCalories <= calories)
+			return true;
+		return false;
+	}
 
-    public void setReady(boolean val) {
-        readyToStart = val;
-    }
+	public int maxCellsPerMove() {
+		return 1;
+	}
 
-    public boolean isReady() {
-        return readyToStart;
-    }
-    
-    public int reduceCalories(int value)
-    {
-    	calories-=value;
-    	return calories;
-    	
-    }
+	public int pointsRemaining() {
+		return -1; // not implemented
+	}
 
-    @Override
-    public void skip(int moves) {
+	public void setReady(boolean val) {
+		readyToStart = val;
+	}
 
-    }
+	public boolean isReady() {
+		return readyToStart;
+	}
 
-    @Override
-    
-    public void putTrap() {
-//        Trap currentTrap = grid.getCurrentTrap();
-        if (trap.getCell() != null)
-            return; // there is an active trap
+	public int reduceCalories(int value) {
+		calories -= value;
+		return calories;
 
-        // TODO: call canPerformEnergyAction(calories)
-        // enough calories to put a trap
-        if (canPerformEnergyAction(Settings.TRAP_REQUIRED_ENERGY)) {
-            System.out.println("Setting Trap");
-            trap.setTrap(getCell(), Settings.TRAP_DURATION, Settings.TRAP_AFFECT_DURATION);
-            calories= reduceCalories(Settings.TRAP_REQUIRED_ENERGY);
-            try {
-                GameAudioPlayer player = new GameAudioPlayer();
-                player.playAudio("place_trap.wav");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+	}
+
+	public int increaseCalories(int value) {
+		calories += value;
+		return calories;
+	}
+
+	public int getCalories() {
+		return calories;
+	}
+
+	@Override
+	public void skip(int moves) {
+
+	}
+
+	@Override
+
+	public void putTrap() {
+		// Trap currentTrap = grid.getCurrentTrap();
+		if (trap.getCell() != null)
+			return; // there is an active trap
+
+		// TODO: call canPerformEnergyAction(calories)
+		// enough calories to put a trap
+		if (canPerformEnergyAction(Settings.TRAP_REQUIRED_ENERGY)) {
+			System.out.println("Setting Trap");
+			trap.setTrap(getCell(), Settings.TRAP_DURATION, Settings.TRAP_AFFECT_DURATION);
+			calories = reduceCalories(Settings.TRAP_REQUIRED_ENERGY);
+			try {
+				GameAudioPlayer player = new GameAudioPlayer();
+				player.playAudio("place_trap.wav");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
